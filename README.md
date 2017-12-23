cb3753958bf044790848b4cc180570d4e6bb082f
Kris De Winter <kdw2060@users.noreply.github.com>
2017-12-23 21:43:13 +0100

    polished mobile lay-out

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-README
+My Alternative for Bose's SoundTouch App
 ======
 
-WORK IN PROGRESS - NOT READY FOR REUSE YET
+WORK IN PROGRESS - NOT FINISHED - HELP WELCOME
+
+This web-app doesn't feature all SoundTouch functions, I just wanted to build something with two features missing from the official app:
+- store more than 6 radio presets 
+- the ability to use the speakers as an intercom
+
+
+
+#Installation and configuration
+==
+1 Download or fork this project
 
 Install with
 
 `npm install`
 
 
-Credits
-https://github.com/watson/bonjour
-https://dansup.github.io/bulma-templates/
-https://freesound.org/people/11linda/sounds/234034/
+#Still to come
+==
+Wanna help out with this project? Here are some TO Do's:
+
+Polish
+- add error handling where missing
+
+Functionality
+- make an array of radio-stations (per user), then use these values in stead of hard-coding them in the html
+- add the ability to search and add stations from within the app
+- in stead of hardcoding the speaker-names, use the Bose API to read out the names and make a dynamic array; also use this to dynamically build the speaker cards and select box
+
+##Some Credits
+I built the lay-out with Bulma css starting from the admin template @ https://dansup.github.io/bulma-templates/
+
+This nice package made it easy to discover the speakers on the local network: https://github.com/watson/bonjour
+
+The sample sound for the intercom function was found here: https://freesound.org/people/11linda/sounds/234034/
--- client/scripts.js
+++ client/scripts.js
@@ -2,20 +2,28 @@
 var speakers;
 var selectedSpeaker = 'SoundTouch10Kris'; //todo: save last selected speaker as a variable in a cookie/localstorage/...
 var selectedSpeakerIP;
-var channelName;
-var APIkey = '';
+var APIkey = '';  //enter your app API key from developer.bose.com
 //List of intercom messages
 var messages = [
     {"name":"dinner", 
     "url": "https://freesound.org/data/previews/234/234034_2631614-lq.mp3"}
 ]
 
-
 //Interface-related functions
 function setSpeaker(speaker, el){
     selectedSpeaker = speaker;
     $("article").removeClass('clicked');
     $(el).toggleClass('clicked');
+    console.log(selectedSpeaker);
+    setSpeakerIP();
+    console.log(selectedSpeakerIP);
+    getInfo();
+};
+
+function setSpeakerViaSelect(event){
+    var selectElement = event.target;
+    var value = selectElement.value;
+    selectedSpeaker = value;
     console.log(selectedSpeaker);
     setSpeakerIP();
     console.log(selectedSpeakerIP);
@@ -93,12 +101,13 @@ function sendIntercomMessage (message) {
 }
 
 function getInfo() {
+      $(".currentSpeaker").html('Selected speaker: <span class="selectedSpeaker">' + selectedSpeaker + '</span>');
       var baseURL = "http://" + selectedSpeakerIP + ":8090";
       var getURL = baseURL + "/now_playing";
       var getURL2 = baseURL + "/volume";
       $.get(getURL, {})
       .done(function(xml){
-          channelName = $(xml).find("itemName").text();
+          var channelName = $(xml).find("itemName").text();
           $('.currentChannel').html('Now playing: <span class="nowPlaying tag is-info">' + channelName + '</span>');
       });
       $.get(getURL2, {})
@@ -110,7 +119,7 @@ function getInfo() {
 }
 
 
-//Event listener and document ready function
+//Event listeners and document ready function
 $(document).ready(function() {
     $.getJSON("http://localhost:3000/api/devices", function(data) {
         speakers = data;
@@ -124,5 +133,21 @@ $(document).ready(function() {
         console.log(volume);
         setVolume(volume);
         setTimeout(getInfo, 500);
-    });   
+    });
+    volumecontroll.addEventListener('touchend', function(){
+        var volume = this.value;
+        console.log(volume);
+        setVolume(volume);
+        setTimeout(getInfo, 500);
+    });
+    
+    var burger = document.getElementById('burger');
+    burger.addEventListener('click', function(){
+            // Get the target from the "data-target" attribute
+            var target = burger.dataset.target;
+            var $target = document.getElementById(target);
+            burger.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+            console.log('hamburger event');
+    });
 });
